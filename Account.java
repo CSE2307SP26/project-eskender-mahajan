aabcc709280b74189e5b26e22f66b47377969427
Beamlak E <b.d.eskender@wust.edu>
2026-03-25 20:06:45 -0500

    Added Test cases

--- Account.java
+++ Account.java
@@ -1,22 +1,23 @@
 public class Account {
-    private double balance; 
+    private double balance;
 
-    public Account(double startingBalance){
-        this.balance = startingBalance; 
+    public Account(double startingBalance) {
+        this.balance = startingBalance;
     }
 
-    public double getBalance(){
-        return balance; 
+    public double getBalance() {
+        return balance;
     }
 
-     public void withdraw(double amount){ 
-        if(amount <= 0){
-            throw new IllegalArgumentException("Withdrawal amount must be positive."); ///throws an except error negative money
-        }
-        if (amount> balance){
-            throw new IllegalArgumentException("Insufficient funds."); /// throws an exception error not enough money 
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be positive.");
         }
 
-        balance -= amount; 
-     }
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds.");
+        }
+
+        balance -= amount;
+    }
 }
