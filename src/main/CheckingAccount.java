c6d732cf072bdae4ba741d4cd8f1b21087725c6c
Beamlak E <b.d.eskender@wust.edu>
2026-04-08 03:21:40 -0500

    checkout

--- src/main/CheckingAccount.java
+++ src/main/CheckingAccount.java
@@ -1,27 +1,33 @@
 package main;
 
-public class CheckingAccount extends BankAccount{
-    private double overdraftLimit; 
- 
-    ///Checking acc takes in starting balance and overdraft limit 
-    public CheckingAccount(double startingBalance, double overdraftLimit){
-        super(startingBalance); 
+public class CheckingAccount extends BankAccount {
+    private double overdraftLimit;
+    private double interestRate;
+
+    // constructor
+    public CheckingAccount(double startingBalance, double overdraftLimit, double interestRate){
+        super(startingBalance);
         this.overdraftLimit = overdraftLimit;
+        this.interestRate = interestRate;
     }
 
-    ////Overrides 
+    // override withdraw
     @Override
     public void withdraw(double amount){
         if(amount <= 0){
             throw new IllegalArgumentException("Withdrawal amount must be positive.");
         }
-         if (amount > getBalance() + overdraftLimit) {
+
+        if (amount > getBalance() + overdraftLimit) {
             throw new IllegalArgumentException("Withdrawal amount exceeds overdraft limit.");
+        }
 
-         } 
-         // If the withdrawal is valid, proceed with the withdrawal
-         super.deposit(-amount); // Use deposit with negative amount to update balance and transaction history
-
+        super.deposit(-amount);
     }
-  
+
+    // apply interest (if required)
+    public void applyInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+    }
 }
