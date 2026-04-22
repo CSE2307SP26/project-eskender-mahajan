1374e3e6f414b9330c0a53de9d3ab4cb78f9e154
Beamlak E <b.d.eskender@wust.edu>
2026-04-22 05:19:37 -0500

    Mortage feature added to consumers

---
+++ src/test/CustomerMortgageTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import main.Customer;
+import main.MortgageApplication;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CustomerMortgageTest {
+
+    @Test
+    void newCustomerStartsWithNoMortgageApplications() {
+        Customer customer = new Customer("Bob");
+
+        assertEquals(0, customer.getMortgageApplications().size());
+    }
+
+    @Test
+    void applyingForMortgageAddsApplication() {
+        Customer customer = new Customer("Bob");
+
+        customer.applyForMortgage(300000, 15);
+
+        assertEquals(1, customer.getMortgageApplications().size());
+    }
+
+    @Test
+    void applyingForMortgageCreatesCorrectApplication() {
+        Customer customer = new Customer("Bob");
+
+        customer.applyForMortgage(300000, 15);
+        MortgageApplication application = customer.getMortgageApplications().get(0);
+
+        assertEquals(customer, application.getCustomer());
+        assertEquals(300000, application.getAmount());
+        assertEquals(15, application.getYears());
+        assertEquals("Pending", application.getStatus());
+    }
+}
